ad5e3a5e355c6d6d394744bea47f31f80d8291a2
pumpson <authergreil@gmail.com>
2022-03-11 21:31:26 +0900

    ggg

---
+++ dbNiTsunaguyo991-1/src/main/java/com/lunacollient/www/DbNiTsunaguyo9911Application.java
@@ -0,0 +1,13 @@
+package com.lunacollient.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbNiTsunaguyo9911Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbNiTsunaguyo9911Application.class, args);
+	}
+
+}
